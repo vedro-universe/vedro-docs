8ce4efd48aff9b4a1b7dd7013ea0677f1d17e2a7
Nikita Tsvetkov <tsv1@fastmail.com>
2025-08-15 23:26:14 +0400

    Refactor font loading in index.js to use link tags for improved performance

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,20 @@ import Head from '@docusaurus/Head';
 import IndexPage from './IndexPage/IndexPage';
 
 export default function Home() {
-  const fonts = `
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400&display=swap');
-  `;
-
   return (
     <>
       <Head>
         <title>Vedro Testing Framework</title>
         <meta name="description" content="Readable. Scalable. Pragmatic." />
-        <style>{fonts}</style>
+
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400&display=swap" 
+          rel="stylesheet" 
+        />
+
         <link rel="stylesheet" href="/css/index.min.css" />
       </Head>
       <main id="vedro-index">
